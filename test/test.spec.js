d86332b028eaf16c3b1b1646d174d0ffb13261cc
GrafRaf <grafraf@gmail.com>
2015-04-17 00:35:48 +0600

    remove node_modules

---
+++ test/test.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('Days360() function tests', function () {
+
+    it('American: Days("29.10.2014","31.10.2014") shoul be 2', function () {
+        var sd1 = new Date(2014, 9, 29);
+        var fd1 = new Date(2014, 9, 31);
+        var result = Days360(sd1,fd1);
+        expect(result).toEqual(2);
+    });
+
+    it('American: Days("30.10.2014","31.10.2014") shoul be 0', function () {
+        var sd1 = new Date(2014, 9, 30);
+        var fd1 = new Date(2014, 9, 31);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(0);
+    });
+
+    it('American: Days("30.10.2014","01.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 9, 30);
+        var fd1 = new Date(2014, 10, 1);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("31.10.2014","01.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 9, 31);
+        var fd1 = new Date(2014, 10, 1);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("01.11.2014","01.11.2014") shoul be 0', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 1);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(0);
+    });
+
+    it('American: Days("01.11.2014","02.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 2);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("01.11.2014","03.11.2014") shoul be 2', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 3);
+        var result = Days360(sd1, fd1);
+        expect(result).toEqual(2);
+    });
+});
