2f141db9c23f5ddde9ade5f58deef82c932bc7e6
GrafRaf <grafraf@gmail.com>
2015-04-17 00:24:08 +0600

    add gulp/karma/jasmine add test

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # days360.js
 days360 excel function for js
+
+## MS function description
+
+
+###DAYS360 function
+This article describes the formula syntax and usage of the DAYS360function in Microsoft Office Excel.
+
+####Description
+
+The DAYS360 function returns the number of days between two dates based on a 360-day year (twelve 30-day months), which is used in some accounting calculations. Use this function to help compute payments if your accounting system is based on twelve 30-day months.
+
+####Syntax
+
+DAYS360(start_date, end_date, [method])
+
+The DAYS360 function syntax has the following arguments:
+
+start_date, end_date    Required. The two dates between which you want to know the number of days. If start_date occurs after end_date, the DAYS360 function returns a negative number. Dates should be entered by using the DATE function, or derived from the results of other formulas or functions. For example, use DATE(2008,5,23) to return the 23rd day of May, 2008. Problems can occur if dates are entered as text.
+
+For more information, see Change the date system, format, or two-digit year interpretation.
+
+method    Optional. A logical value that specifies whether to use the U.S. or European method in the calculation.
+
+
+Method
+
+Defined
+
+FALSE or omitted
+
+U.S. (NASD) method. If the starting date is the last day of a month, it becomes equal to the 30th day of the same month. If the ending date is the last day of a month and the starting date is earlier than the 30th day of a month, the ending date becomes equal to the 1st day of the next month; otherwise the ending date becomes equal to the 30th day of the same month.
+
+TRUE
+
+European method. Starting dates and ending dates that occur on the 31st day of a month become equal to the 30th day of the same month.
+
+NOTE   Excel stores dates as sequential serial numbers so that they can be used in calculations. By default, January 1, 1900 is serial number 1, and January 1, 2008 is serial number 39448 because it is 39,447 days after January 1, 1900. Microsoft Excel for the Macintosh uses a different date system as its default.
+
+For more information, see Change the date system, format, or two-digit year interpretation.
+
+
+https://support.office.com/en-us/article/DAYS360-function-c80baf88-68b5-464e-ad63-2215912eca43?CTT=3&CorrelationId=d71015c7-2053-4bea-999e-be08d503fb3c&ui=en-US&rs=en-US&ad=US
---
+++ src/days360.js
@@ -0,0 +1,101 @@
+﻿'use strict';
+
+function Days360(sd, fd, m) {
+    var d1 = new Date(sd);
+    var d2 = new Date(fd);
+    var d1_1 = d1;
+    var d2_1 = d2;
+    var method = m || false;
+    var d1_y = d1.getFullYear();
+    var d2_y = d2.getFullYear();
+    var dy = 0;
+    var d1_m = d1.getMonth();
+    var d2_m = d2.getMonth();
+    var dm = 0;
+    var d1_d = d1.getDate();
+    var d2_d = d2.getDate();
+    var dd = 0;
+    var sign = 1.0;
+    if (method) {
+        // euro
+        if (d1_d == 31) d1_d = 30;
+        if (d2_d == 31) d2_2 = 30;
+    } else {
+        // american NASD
+        if (d1_d == 31) d1_d = 30;
+        if (d2_d == 31) {
+            if (d1_d < 30) {
+                if (d2_m == 11) {
+                    d2_y = d2_y + 1;
+                    d2_m = 0;
+                    d2_d = 1;
+                } else {
+                    d2_m = d2_m + 1;
+                    d2_d = 1;
+                }
+            } else {
+                d2_d = 30;
+            }
+        }
+    }
+    dy = d2_y - d1_y;
+    dm = d2_m - d1_m;
+    dd = d2_d - d1_d;
+    //d1_d = d1_1 - d1_1.getTimezoneOffset() * 1000;
+    //d2_d = d2_1 - d2_1.getTimezoneOffset() * 1000;
+    return parseFloat(dy * 360 + dm * 30 + dd);
+}
+
+/*
+    var sd1 = new Date(2014, 9, 29);
+    var fd1 = new Date(2014, 9, 31);
+    console.assert(Days360(sd1, fd1) == 2, "1 NeOk");
+
+    var sd2 = new Date(2014, 9, 30);
+    var fd2 = new Date(2014, 9, 31);
+    console.assert(Days360(sd2, fd2) == 0, "2 NeOk");
+
+    var sd3 = new Date(2014, 9, 31);
+    var fd3 = new Date(2014, 10, 1);
+    console.assert(Days360(sd3, fd3) == 1, "3 NeOk");
+
+    var sd4 = new Date(2014, 9, 31);
+    var fd4 = new Date(2014, 10, 1);
+    console.assert(Days360(sd4, fd4) == 1, "4 NeOk");
+
+    var sd5 = new Date(2014, 10, 1);
+    var fd5 = new Date(2014, 10, 1);
+    console.assert(Days360(sd5, fd5) == 0, "5 NeOk");
+
+    var sd6 = new Date(2014, 10, 1);
+    var fd6 = new Date(2014, 10, 2);
+    console.assert(Days360(sd6, fd6) == 1, "6 NeOk");
+
+    var sd7 = new Date(2014, 10, 1);
+    var fd7 = new Date(2014, 10, 3);
+    console.assert(Days360(sd7, fd7) == 2, "7 NeOk");
+*/
+/*
+American
+29.10.2014       2  // 1
+31.10.2014       
+         
+30.10.2014       0  // 2
+31.10.2014       
+         
+30.10.2014       1  // 3
+01.11.2014       
+         
+31.10.2014       1  // 4
+01.11.2014       
+         
+01.11.2014       0  // 5
+01.11.2014       
+         
+01.11.2014       1  // 6
+02.11.2014       
+         
+01.11.2014       2  // 7
+03.11.2014       
+*/
+
