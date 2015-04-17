e3d735af12c1de8c881a7340d4707f9efe45524a
GrafRaf <grafraf@gmail.com>
2015-04-17 18:33:13 +0600

    md fixes add ararog. version of days360 function and test for it

---
+++ src/Days360_ararog.js
@@ -0,0 +1,31 @@
+﻿function days360(initialDate, currentDate) {
+
+    var dateA = initialDate;
+
+    var dateB = currentDate;
+
+    var dayA = dateA.getDate();
+
+    var dayB = dateB.getDate();
+
+    if (lastDayOfFebruary(dateA) && lastDayOfFebruary(dateB))
+        dayB = 30;
+
+    if (dayA == 31 && lastDayOfFebruary(dateA))
+        dayA = 30;
+
+    if (dayA == 30 && dayB == 31)
+        dayB = 30;
+
+    days = (dateB.getFullYear() - dateA.getFullYear()) * 360 +
+        ((dateB.getMonth() + 1) - (dateA.getMonth() + 1)) * 30 + (dayB - dayA);
+
+    return days;
+}
+
+function lastDayOfFebruary(date) {
+
+    lastDay = new Date(date.getFullYear(), 2, -1);
+
+    return date.getDate() == lastDay;
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -51,3 +51,55 @@ describe('Days360() function tests', function () {
         expect(result).toEqual(2);
     });
 });
+
+describe('Ararog bersion of Days360() function tests', function () {
+
+    it('American: Days("29.10.2014","31.10.2014") shoul be 2', function () {
+        var sd1 = new Date(2014, 9, 29);
+        var fd1 = new Date(2014, 9, 31);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(2);
+    });
+
+    it('American: Days("30.10.2014","31.10.2014") shoul be 0', function () {
+        var sd1 = new Date(2014, 9, 30);
+        var fd1 = new Date(2014, 9, 31);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(0);
+    });
+
+    it('American: Days("30.10.2014","01.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 9, 30);
+        var fd1 = new Date(2014, 10, 1);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("31.10.2014","01.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 9, 31);
+        var fd1 = new Date(2014, 10, 1);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("01.11.2014","01.11.2014") shoul be 0', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 1);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(0);
+    });
+
+    it('American: Days("01.11.2014","02.11.2014") shoul be 1', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 2);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(1);
+    });
+
+    it('American: Days("01.11.2014","03.11.2014") shoul be 2', function () {
+        var sd1 = new Date(2014, 10, 1);
+        var fd1 = new Date(2014, 10, 3);
+        var result = days360(sd1, fd1);
+        expect(result).toEqual(2);
+    });
+});
